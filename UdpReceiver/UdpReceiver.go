40f13f3d5ecb86ed10e4214879589aac4227db77
insang1983 <insang1983@gmail.com>
2016-10-21 08:59:20 +0900

    UdpReceiver v0.1

--- UdpReceiver/UdpReceiver.go
+++ UdpReceiver/UdpReceiver.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"net"
 	"time"
-	//"bufio"
+	"bufio"
 )
 
 /* A Simple function to verify error */
@@ -26,62 +26,70 @@ func main() {
 	CheckError(err)
 	defer ServerConn.Close()
 
-	buf := make([]byte, 1024)
-	var file *os.File = nil
+	yyyymmddhh24mi := "200601021504"
+	yyyymmddhh24miss := "20060102150405"
+	yyyymmdd_hh24miss := "20060102 150405"
+
+	buf := make([]byte, 4096)
+	var recv_msg string
 	var iCount int = 0
-	prevTime := time.Now().Local()
+
+	now := time.Now().Local()
+	before := now
+	prevTime := now
+
+	filename := now.Format(yyyymmddhh24mi) + ".txt"
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.FileMode(0666))
+	if err != nil {
+		fmt.Println(now.Format(yyyymmdd_hh24miss),err)
+		os.Exit(2)
+	}
+	fmt.Println(now.Format(yyyymmdd_hh24miss),filename, "is opend..")
+	file_obj := bufio.NewWriter(file)
+	file.Seek(0, os.SEEK_END)
 
 	for {
-		now := time.Now().Local()
+		now = time.Now()
 
-		if now.Unix() - prevTime.Unix() >= 1 {
-			fmt.Println(now.Format("20060102 150405"), "eps : ", iCount )
+		if now.Local().Format(yyyymmddhh24miss) != prevTime.Local().Format(yyyymmddhh24miss) {
+			fmt.Println(now.Format(yyyymmdd_hh24miss), "eps : ", iCount )
 			iCount = 0
 			prevTime = now
 		}
 
-		filename := now.Format("200601021504") + ".txt"
-		if _, err := os.Stat(filename); os.IsNotExist(err){
+		if now.Local().Format(yyyymmddhh24mi) != before.Local().Format(yyyymmddhh24mi) {
+			before = now
 			if file != nil {
 				file.Close()
 				file = nil
 			}
-			file, err = os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, os.FileMode(0644))
+			filename := now.Format(yyyymmddhh24mi) + ".txt"
+			file, err = os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.FileMode(0666))
 			if err != nil {
-				fmt.Println(err)
+				fmt.Println(now.Format(yyyymmdd_hh24miss), err)
 				continue 
 			}
-		} else {
-			//있으면 열렸는지?
-			//열렸으면 close 하고 open
-			if file == nil {
-				file, err = os.OpenFile(filename, os.O_WRONLY, os.FileMode(0644))
-				if err != nil {
-					fmt.Println(err)
-					//return
-					continue
-				}
-			} 
 			file.Seek(0, os.SEEK_END)
+			file_obj = bufio.NewWriter(file)
+			fmt.Println(now.Format(yyyymmdd_hh24miss), filename, "is opend..")
 		}
 
-		ServerConn.SetReadDeadline(time.Now().Add( 1*time.Nanosecond))
+		ServerConn.SetReadDeadline(time.Now().Add(1*time.Second))
 		n, addr, err := ServerConn.ReadFromUDP(buf)
 		if err != nil {
 			if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-				//timeout
-				continue;
+				continue; //timeout
 			} else {
-				fmt.Println("Error: ", err)
+				fmt.Println(now.Format(yyyymmdd_hh24miss), "Error: ", err)
 			}
 		}
 
-		iCount++ // 0인 경우를 위해 먼저 건수 증가
+		iCount++
 
 		if n > 0 {
-			recv_msg := "["+ addr.IP.String()+ "]["+ now.Format("20060102 150405") + "]["+ string(buf[0:n])+ "]"
-			file.WriteString(recv_msg + "\r\n")
+			recv_msg = "["+ addr.IP.String()+ "]["+ now.Format(yyyymmdd_hh24miss) + "]["+ string(buf[0:n])+ "]"
+			file_obj.WriteString(recv_msg + "\r\n")
+			//file_obj.Flush()
 		}
-
 	}
 }
